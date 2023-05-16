ac39c6815c36cb776f27fae266bbdaa7e9040415
SurendarKumarS <surendarkumar.s.21@gmail.com>
2023-05-16 16:00:01 +0530

    addr

--- src/org/accessspecifier/AccessSpecifier.java
+++ src/org/accessspecifier/AccessSpecifier.java
@@ -5,6 +5,9 @@ public class AccessSpecifier {
 	public void empName() {
 		System.out.println("Employee name is Kumar");
 	}
-	
+	public void empAdd() {
+		System.out.println("Employee in chennai");
+
+	}
 
 }
